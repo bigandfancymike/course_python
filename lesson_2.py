475206a1dcbba39d3e1a4d7475ad7337dcaa7d64
bigandfancymike <90044731+bigandfancymike@users.noreply.github.com>
2024-02-29 20:44:10 +0200

    hello world

--- lesson_2.py
+++ lesson_2.py
@@ -100,6 +100,6 @@ t = (1, 2, 3)
 print('list', sys.getsizeof(l), sep='=')
 print('tuple', sys.getsizeof(l), sep='=')
 
-#hello world
+#hello worldфів
 
 git clone https://bigandfancymike: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@github.com/username/repo.git
