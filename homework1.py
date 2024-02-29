35b766027bcf4cbc311be82c691c3f17bd23540f
bigandfancymike <90044731+bigandfancymike@users.noreply.github.com>
2024-02-29 19:41:48 +0200

    hello world

---
+++ homework1.py
@@ -0,0 +1,19 @@
+a = 10
+b = 15
+c = 25
+
+my_name = 'Michael'
+x_occupation = 'Manual_qa'
+var_city = 'Cherkasy'
+
+LASTNAME = 'Iliashenko'
+MY_SHOE_SIZE = 45
+
+#hello world + lorem ipsum
+
+"""
+print
+hello
+world
+!
+"""
