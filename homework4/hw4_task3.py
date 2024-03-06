ff2eec4b87325079c6c1875cc47690dbb3248ae8
bigandfancymike <michael.iliashenko@gmail.com>
2024-03-06 21:21:57 +0200

    homework4 iliashenko

---
+++ homework4/hw4_task3.py
@@ -0,0 +1,8 @@
+# task3 - Створіть словник з чотирма назвами мов програмування (ключі) та іменами розробників цих мов (значення). Виведіть по черзі для усіх елементів словника повідомлення типу My favorite programming language is Python. It was created by Guido van Rossum.. Видаліть, на ваш вибір, одну пару «мова: розробник» із словника. Виведіть словник на екран.
+
+dictionary1 = {'java': 'James Gosling', 'js': 'Brendan Eich', 'ruby': 'Yukihiro Matsumoto', 'python': 'Guido van Rossum'}
+
+for language, developer in dictionary1.items():
+    print(f'My favorite programming language is Python. It was created by Guido van Rossum.')
+del dictionary1['js']
+print('Updated one:', dictionary1)
