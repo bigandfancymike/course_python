063e37d493c62d56a7cbaafa6dab1f09937bf4c9
bigandfancymike <michael.iliashenko@gmail.com>
2024-03-08 16:22:47 +0200

    fixed previos mistakes

--- lesson_4/hw4_task2.py
+++ lesson_4/hw4_task2.py
@@ -1,15 +1,17 @@
 # task2 - У вас є список змінних у форматі CamelCase ["FirstItem", "FriendsList", "MyTuple"] , перетворити його у список змінних для Пайтона snace_case, "friends_list", "my_tuple"]
 
-camel_case_list = ["FirstItem", "FriendsList", "MyTuple"]
-snake_case_list = [item[0].lower() + item[1:].replace('I', '_i').replace('L', '_l').replace('T', '_t').lower() for item in camel_case_list]
+camel_case_list = ["FirstItem", "FriendsList", "MyTuple", "WonMatch"]
+snake_case_list = []
+
+for item in camel_case_list:
+    snake_case_word = ""
+    for i in range(len(item)):
+        if item[i].isupper() and i != 0:
+            snake_case_word += '_' + item[i].lower()
+        else:
+            snake_case_word += item[i].lower()
+    snake_case_list.append(snake_case_word)
 print(snake_case_list)
-
-
-
-# l = ['NewCase']
-# new_case = l[0][0].lower() + l [0][1:].replace('C', '_c')
-# print(new_case)
-
 
 
 
