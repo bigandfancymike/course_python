71e2d1ca20039e79c1c288dd5b7cd03efd251f13
bigandfancymike <michael.iliashenko@gmail.com>
2024-03-14 19:22:26 +0200

    lesson_6

---
+++ lesson_6/annotations.py
@@ -0,0 +1,11 @@
+def say_hi(name: str) -> str:
+    name.
+    if isinstance(name, int):
+        print('This is string')
+    print('This is something else')
+    return 'from return'
+
+say_hi('str', 10)
+
+
+21:05
---
+++ lesson_6/math_operations.py
@@ -0,0 +1,24 @@
+#
+# def plus(a, b):
+#     pass
+#
+# def minus(a, b):
+#     pass
+#
+#
+# def click_on_element(xpath):
+#     pass
+#
+#
+# def test_login():
+#     click_on_element()
+#     type_text()
+#     type_text()
+#     click_on_element()
+#
+#
+# # def test_login():
+# #     driver.find_element('XPATH_1').click() #click start login
+# #     driver.find_element('XPATH').send_keys('email')
+# #     driver.find_element('XPATH').send_keys('pwd')
+# #     driver.find_element('XPATH').click()
